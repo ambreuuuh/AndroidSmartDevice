04fe734d02fb923377d554db4cbe323b8360f971
ambreuuuh <102356244+ambreuuuh@users.noreply.github.com>
2023-03-27 09:01:10 +0200

    authorisation

--- app/src/main/java/fr/isen/aurelien/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/aurelien/androidsmartdevice/ScanActivity.kt
@@ -12,11 +12,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.isen.aurelien.androidsmartdevice.databinding.ActivityScanBinding
+import android.Manifest.permission.*
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class ScanActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityScanBinding
     private var mScanning = false
+    val bluetoothPermission = BLUETOOTH_CONNECT
+    companion object {
+        private const val REQUEST_BLUETOOTH_PERMISSION=1
+    }
 
     private val bluetoothAdapter: BluetoothAdapter? by
     lazy(LazyThreadSafetyMode.NONE) {
@@ -26,28 +34,47 @@ class ScanActivity : AppCompatActivity(){
     }
 
 
+    private val REQUIRED_PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        arrayOf(
+            BLUETOOTH_CONNECT,
+            BLUETOOTH_SCAN
+        )
+    } else {
+        arrayOf(
+            BLUETOOTH,
+            BLUETOOTH_ADMIN
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScanBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val bluetoothLeScanner = bluetoothAdapter!!.bluetoothLeScanner
+        checkSelfPermission()
+        startScan(bluetoothLeScanner)
 
-        if (bluetoothAdapter?.isEnabled == true){
-            //j'ai le BLE
-        } else {
-            // je ne l'ai pas
-        }
-        binding.textlancement.setOnClickListener{
-           togglePlayPauseAction()
-        }
-        binding.play.setOnClickListener{
-           togglePlayPauseAction()
-        }
 
         binding.recyclerview.layoutManager = LinearLayoutManager(this)
         binding.recyclerview.adapter = ScanAdapter(arrayListOf("Device 1", "Device 2"))
     }
-    
 
+    private fun startScan(bluetoothLeScanner: BluetoothLeScanner) {
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth non disponible", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (bluetoothAdapter?.isEnabled == false) {
+            Toast.makeText(this, "Bluetooth non activé", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (bluetoothAdapter?.isEnabled == true) {
+            binding.play.setOnClickListener() {
+                togglePlayPauseAction()
+                //initList()
+            }
+        }
+    }
     private fun togglePlayPauseAction(){
         mScanning= !mScanning
         if(mScanning){
@@ -61,25 +88,17 @@ class ScanActivity : AppCompatActivity(){
         }
 
     }
-    /*private fun startScan(bluetoothLeScanner: BluetoothLeScanner) {
-        if (bluetoothAdapter == null) {
-            Toast.makeText(this, "Bluetooth non disponible", Toast.LENGTH_SHORT).show()
-            return
-        }
-        if (bluetoothAdapter?.isEnabled == false) {
-            Toast.makeText(this, "Bluetooth non activé", Toast.LENGTH_SHORT).show()
-            return
-        }
-        if (bluetoothAdapter?.isEnabled == true) {
-            binding.play.setOnClickListener() {
-                //togglePlayPauseAction()
-                //initList()
-            }
-        }
-    }*/
+    private fun checkSelfPermission()  {
+        if (REQUIRED_PERMISSIONS.all {
+                ContextCompat.checkSelfPermission(this, bluetoothPermission) != PackageManager.PERMISSION_GRANTED})
+            // Demander l'autorisation Bluetooth
+            ActivityCompat.requestPermissions(this, arrayOf(bluetoothPermission), REQUEST_BLUETOOTH_PERMISSION)
+
+    }
 
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             REQUEST_BLUETOOTH_PERMISSION -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -90,8 +109,8 @@ class ScanActivity : AppCompatActivity(){
                     // Afficher un message à l'utilisateur pour l'informer que l'application a besoin de cette autorisation pour fonctionner correctement
                     Toast.makeText(this, "Vous avez besoin du bluetooth pour utiliser l'application", Toast.LENGTH_SHORT).show()
                 }
+                return
             }
-            return
         }
     }
 
